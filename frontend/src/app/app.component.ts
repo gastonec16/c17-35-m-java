0ad4a43f1cfa6b7a3234785c996892de704e7255
FrancoNordinelli <imnordi@gmail.com>
2024-04-02 16:52:03 -0300

    integration test

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,16 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  //TEST DE INTEGRACIóN SPRING ANGULAR
   title = 'frontend';
+  constructor(private http: HttpClient) { }
+
+    ngOnInit() {
+        this.http.get<any>('/api/test').subscribe(response => {
+            this.title = response;
+        });
+    }
 }
+
+
+
