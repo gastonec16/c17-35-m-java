7e15ec188f2c5f72d6d938ddd68d14f41ce9479f
FrancoNordinelli <imnordi@gmail.com>
2024-04-03 13:31:20 -0300

    fix

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-=======
 import { Component } from '@angular/core'
 import { RouterModule } from '@angular/router'
->>>>>>> main
 
 @Component({
     selector: 'app-root',
@@ -14,22 +8,6 @@ import { RouterModule } from '@angular/router'
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
-<<<<<<< HEAD
-export class AppComponent implements OnInit {
-  title = 'frontend';
-  messageFromBackend :string
-  constructor(private http: HttpClient) {
-    this.messageFromBackend = ''
-   }
-
-  ngOnInit() {
-      this.http.get<any>('http://localhost:8080/api/test').subscribe( (response : any )=> {
-          this.messageFromBackend = response;
-      });
-  }
-
-=======
 export class AppComponent {
     title = 'frontend'
->>>>>>> main
 }
