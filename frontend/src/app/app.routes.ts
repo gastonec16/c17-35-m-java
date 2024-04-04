98055d2c0bcec273b307d367ee9ee66b609457af
Gastón Cuello <gastonec16@gmail.com>
2024-04-04 10:24:05 -0300

    plantilla registrarse

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,5 +4,10 @@ export const routes: Routes = [
     {
         path: 'log-in',
         loadComponent: () => import('./components/log-in/log-in.component').then((c) => c.LogInComponent)
+    },
+    {
+        path: 'register',
+        loadComponent: () =>
+            import('./components/register/register.component').then((c) => c.RegisterComponent)
     }
 ]
