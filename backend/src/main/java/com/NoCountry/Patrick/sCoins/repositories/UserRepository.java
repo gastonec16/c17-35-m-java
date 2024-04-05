820490a56199ebd4b621f95908fc9af40dfdd847
FrancoNordinelli <imnordi@gmail.com>
2024-04-05 01:10:09 -0300

    userRepositry

---
+++ backend/src/main/java/com/NoCountry/Patrick/sCoins/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.NoCountry.Patrick.sCoins.repositories;
+
+import com.NoCountry.Patrick.sCoins.entities.User;
+
+//TO DO: IMPLEMENTAR JPA
+public interface UserRepository {
+
+    //TO DO
+    User save(User user);
+    
+}
