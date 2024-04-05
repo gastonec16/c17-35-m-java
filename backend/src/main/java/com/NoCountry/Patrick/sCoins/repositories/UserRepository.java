36fc26fa9836d514bc3080ee69b37352f94c5912
FrancoNordinelli <imnordi@gmail.com>
2024-04-05 10:31:52 -0300

    JPA

--- backend/src/main/java/com/NoCountry/Patrick/sCoins/repositories/UserRepository.java
+++ backend/src/main/java/com/NoCountry/Patrick/sCoins/repositories/UserRepository.java
@@ -1,11 +1,16 @@
 package com.NoCountry.Patrick.sCoins.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.NoCountry.Patrick.sCoins.entities.User;
 
-//TO DO: IMPLEMENTAR JPA
-public interface UserRepository {
 
-    //TO DO
-    User save(User user);
+
+//TO DO: IMPLEMENTAR JPA
+public interface UserRepository extends JpaRepository<User,Long>{
+
+    //TO DO: SALVAR EN BD
+    @SuppressWarnings("unchecked")
+    public User save(User user);
     
 }
