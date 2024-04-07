74b23bc541dd1e4071f0676ee99bb54dba5034d9
FrancoNordinelli <imnordi@gmail.com>
2024-04-07 18:05:05 -0300

    cors OK

--- backend/src/main/java/com/NoCountry/Patrickscoin/controller/RegisterController.java
+++ backend/src/main/java/com/NoCountry/Patrickscoin/controller/RegisterController.java
@@ -4,18 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.NoCountry.Patrickscoin.entities.User;
 import com.NoCountry.Patrickscoin.models.UserModel;
 import com.NoCountry.Patrickscoin.services.IRegisterService;
 
+
+
+
+
 @RestController
-@RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200") // Permitir solicitudes desde localhost:4200
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
+
 public class RegisterController {
     
 
@@ -24,9 +30,10 @@ public class RegisterController {
     @Autowired
     private IRegisterService userService;
 
-    @PostMapping("/users")
+
+    
+    @PostMapping("/api/users")
     public ResponseEntity<User> registerUser(@RequestBody UserModel userModel){
-        System.err.println("INFO DE SOLICITUD DE REGISTRO");
         User registeredUser = userService.registerUser(userModel);
         return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
     }
