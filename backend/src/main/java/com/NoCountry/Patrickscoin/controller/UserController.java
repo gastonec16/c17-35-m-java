2a836e94aeefcc0881a4ea8b0ecbf14cebc679e8
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-04-09 11:49:44 -0300

    create: endpoint para buscar por id en UserController

--- backend/src/main/java/com/NoCountry/Patrickscoin/controller/UserController.java
+++ backend/src/main/java/com/NoCountry/Patrickscoin/controller/UserController.java
@@ -3,6 +3,8 @@ package com.NoCountry.Patrickscoin.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +25,10 @@ public class UserController {
     public ResponseEntity<User> registerUser(@RequestBody UserDto userdto){
         User registeredUser = userService.registerUser(userdto);
         return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable Long id) throws Exception{
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 }
