6e6fda8a6ce7ca812cd62cc57b18e4610a30cc51
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-04-09 11:21:39 -0300

    refact: UserController

--- backend/src/main/java/com/NoCountry/Patrickscoin/controller/UserController.java
+++ backend/src/main/java/com/NoCountry/Patrickscoin/controller/UserController.java
@@ -1,21 +1,27 @@
 package com.NoCountry.Patrickscoin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.NoCountry.Patrickscoin.dto.UserDto;
 import com.NoCountry.Patrickscoin.entities.User;
-import com.NoCountry.Patrickscoin.services.UserService;
+import com.NoCountry.Patrickscoin.services.IUserService;
 
+@RestController
+@RequestMapping("/api")
 public class UserController {
     
+    @Autowired
+    private IUserService userService;
 
-    private UserService userService;
-
-    @PostMapping ("/register")
-    public ResponseEntity<User> registerUser(@RequestBody User user){
-        User registeredUser = userService.registerUser(user);
+    @PostMapping ("/users")
+    public ResponseEntity<User> registerUser(@RequestBody UserDto userdto){
+        User registeredUser = userService.registerUser(userdto);
         return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
     }
 }
