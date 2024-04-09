3edcb0c754a1906c8fa509a7f5e109398c0e873f
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-04-08 21:53:25 -0300

    remove: anotacion cors de RegisterController

--- backend/src/main/java/com/NoCountry/Patrickscoin/controller/RegisterController.java
+++ backend/src/main/java/com/NoCountry/Patrickscoin/controller/RegisterController.java
@@ -3,35 +3,20 @@ package com.NoCountry.Patrickscoin.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.NoCountry.Patrickscoin.entities.User;
 import com.NoCountry.Patrickscoin.models.UserModel;
 import com.NoCountry.Patrickscoin.services.IRegisterService;
 
-
-
-
-
 @RestController
-@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
-
 public class RegisterController {
     
-
-    
-
     @Autowired
     private IRegisterService userService;
 
-
-    
     @PostMapping("/api/users")
     public ResponseEntity<User> registerUser(@RequestBody UserModel userModel){
         User registeredUser = userService.registerUser(userModel);
