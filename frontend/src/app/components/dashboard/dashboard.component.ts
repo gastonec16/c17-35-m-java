2b1157a2061a25bdfb78a94fb6f912f204ede213
Gastón Cuello <gastonec16@gmail.com>
2024-04-10 14:04:47 -0300

    detalles diseño

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -16,14 +16,14 @@ export class DashboardComponent {
         id: 34,
         name: 'David',
         lastName: 'Reyes',
-        email: 'davidreyes@hotmail.comaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        email: 'davidreyes@hotmail.com',
         ars: 95123,
         usd: 215,
         crypto: [
             { id: 1, name: 'Bitcoin', shortName: 'BTC', quantity: 0.26 },
             { id: 2, name: 'Ethereum', shortName: 'ETH', quantity: 0.65 },
             { id: 3, name: 'USDT', shortName: 'USDT', quantity: 125 },
-            { id: 4, name: 'BNB', shortName: 'BNB', quantity: 0.32 },
+            { id: 4, name: 'BNB', shortName: 'BNB', quantity: 0 },
             { id: 5, name: 'Solana', shortName: 'SOL', quantity: 2.6 },
             { id: 6, name: 'Ripple', shortName: 'XRP', quantity: 3.6 },
             { id: 7, name: 'Dogecoin', shortName: 'DOGE', quantity: 666 }
