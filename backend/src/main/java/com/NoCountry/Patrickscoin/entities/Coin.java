949e22e1f86648e2ee3665bf0c76c6fe247f4406
Alan Kennedy <Alaneduardokennedy@gmail.com>
2024-04-12 12:18:59 -0300

    add Wallet

--- backend/src/main/java/com/NoCountry/Patrickscoin/entities/Coin.java
+++ backend/src/main/java/com/NoCountry/Patrickscoin/entities/Coin.java
@@ -2,9 +2,12 @@ package com.NoCountry.Patrickscoin.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,4 +28,8 @@ public class Coin {
     private String name;
     @Column(nullable = false)
     private String quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "wallet.id")
+    private Wallet wallet;
 }
