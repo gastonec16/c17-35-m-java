fe88c208118c6bddee457bc1dac7252ed70aedc0
FrancoNordinelli <imnordi@gmail.com>
2024-04-13 14:26:22 -0300

    relación con wallet

--- backend/src/main/java/com/NoCountry/Patrickscoin/entities/User.java
+++ backend/src/main/java/com/NoCountry/Patrickscoin/entities/User.java
@@ -1,10 +1,12 @@
 package com.NoCountry.Patrickscoin.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -30,4 +32,6 @@ public class User {
     private String email;
     @Column(nullable = false)
     private String password;
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Wallet wallet;
 }
