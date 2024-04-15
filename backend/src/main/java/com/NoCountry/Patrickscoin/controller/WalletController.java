557bc5d8fb108430289a3e11b866fbc9549d730f
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-04-15 20:12:21 -0300

    create: wallet controller basaico

--- backend/src/main/java/com/NoCountry/Patrickscoin/controller/WalletController.java
+++ backend/src/main/java/com/NoCountry/Patrickscoin/controller/WalletController.java
@@ -1,5 +1,6 @@
 package com.NoCountry.Patrickscoin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,16 +8,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.NoCountry.Patrickscoin.services.IWalletService;
+
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/wallet")
 public class WalletController {
 
-    /* @PostMapping("/")
+    @Autowired
+    private IWalletService  walletService; 
+
+    @PostMapping("/")
     public ResponseEntity<?> depositFiat(@PathVariable("walletId") Long walletId, @RequestBody DesposiDto depositDto){
         walletService.deposit(walletId, depositDto);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    } */
+    } 
     
 }
