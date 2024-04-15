c9210e98ce84869074a3f35ebed3f211f332e568
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-04-15 20:12:59 -0300

    create: wallet servicios basico

--- backend/src/main/java/com/NoCountry/Patrickscoin/services/IWalletService.java
+++ backend/src/main/java/com/NoCountry/Patrickscoin/services/IWalletService.java
@@ -1,7 +1,10 @@
 package com.NoCountry.Patrickscoin.services;
 
 import com.NoCountry.Patrickscoin.dto.DepositDto;
+import com.NoCountry.Patrickscoin.entities.Wallet;
+import com.NoCountry.Patrickscoin.exception.WalletException;
 
 public interface IWalletService {
     public void deposit(Long walletId, DepositDto depositDto);
+    public Wallet findById(Long walletId) throws WalletException;
 }
--- backend/src/main/java/com/NoCountry/Patrickscoin/services/implementation/WalletService.java
+++ backend/src/main/java/com/NoCountry/Patrickscoin/services/implementation/WalletService.java
@@ -1,17 +1,28 @@
 package com.NoCountry.Patrickscoin.services.implementation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.NoCountry.Patrickscoin.dto.DepositDto;
+import com.NoCountry.Patrickscoin.entities.Wallet;
+import com.NoCountry.Patrickscoin.exception.WalletException;
+import com.NoCountry.Patrickscoin.repositories.WalletRepository;
 import com.NoCountry.Patrickscoin.services.IWalletService;
 
 @Service
 public class WalletService implements IWalletService{
 
+    @Autowired
+    private WalletRepository walletRepository;
+
     @Override
     public void deposit(Long walletId, DepositDto depositDto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deposit'");
+        
     }
-    
+
+    @Override
+    public Wallet findById(Long walletId) throws WalletException {
+        return walletRepository.findById(walletId).orElseThrow(()-> new WalletException("Wallet noencontrada"));;
+    }
+
 }
