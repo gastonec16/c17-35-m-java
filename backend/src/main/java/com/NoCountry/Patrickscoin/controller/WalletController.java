0e8cc3ef59bbdf4ab28db1edfe9a9a6aac5c0829
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-04-16 13:56:15 -0300

    create: metodo deposit y optener por id

--- backend/src/main/java/com/NoCountry/Patrickscoin/controller/WalletController.java
+++ backend/src/main/java/com/NoCountry/Patrickscoin/controller/WalletController.java
@@ -5,12 +5,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.NoCountry.Patrickscoin.dto.DepositDto;
+import com.NoCountry.Patrickscoin.exception.WalletException;
 import com.NoCountry.Patrickscoin.services.IWalletService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @RequestMapping("/api/wallet")
@@ -19,8 +23,13 @@ public class WalletController {
     @Autowired
     private IWalletService  walletService; 
 
-    @PostMapping("/")
-    public ResponseEntity<?> depositFiat(@PathVariable("walletId") Long walletId, @RequestBody DesposiDto depositDto){
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getWalletById(@PathVariable Long id) throws WalletException{
+        return ResponseEntity.ok().body(walletService.findById(id));
+    }
+
+    @PostMapping("/{walletId}")
+    public ResponseEntity<?> depositFiat(@PathVariable Long walletId, @RequestBody DepositDto depositDto) throws WalletException{
         walletService.deposit(walletId, depositDto);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     } 
