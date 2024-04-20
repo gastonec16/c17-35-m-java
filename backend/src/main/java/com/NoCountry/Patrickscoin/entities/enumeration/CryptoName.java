8734f92045d8d980412b14a4ac49d4303dc86cf6
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-04-20 04:44:44 -0300

    create: enum para representar los nombres de las criptosvalidas

---
+++ backend/src/main/java/com/NoCountry/Patrickscoin/entities/enumeration/CryptoName.java
@@ -0,0 +1,5 @@
+package com.NoCountry.Patrickscoin.entities.enumeration;
+
+public enum CryptoName {
+    BTC, ETH, USDT, USDC, DAI, UXD, USDP, WLD, BNB, SOL, XRP, ADA, AVAX, DOGE, TRX, LINK, DOT, MATIC, SHIB, LTC, BCH, EOS, XLM, FTM, AAVE, UNI, ALGO, BAT, PAXG, CAKE, AXS, SLP, MANA, SAND, CHZ
+}
