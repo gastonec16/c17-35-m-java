017824e41c9b58582b394866da61872d517f59aa
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-04-20 02:22:43 -0300

    create: metodo withdraw que verifica y quita el saldo a retirar de la wallet

--- backend/src/main/java/com/NoCountry/Patrickscoin/services/implementation/WalletService.java
+++ backend/src/main/java/com/NoCountry/Patrickscoin/services/implementation/WalletService.java
@@ -1,9 +1,13 @@
 package com.NoCountry.Patrickscoin.services.implementation;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.NoCountry.Patrickscoin.dto.request.DepositDto;
+import com.NoCountry.Patrickscoin.dto.request.WithdrawDto;
+import com.NoCountry.Patrickscoin.dto.response.TicketWithdrawDtoResponse;
 import com.NoCountry.Patrickscoin.entities.Wallet;
 import com.NoCountry.Patrickscoin.entities.enumeration.MoneyType;
 import com.NoCountry.Patrickscoin.exception.WalletException;
@@ -31,6 +35,42 @@ public class WalletService implements IWalletService{
         if(depositDto.getName().equalsIgnoreCase(MoneyType.USD.name()))
             wallet.setGlobalMoney(depositDto.getAmount());
 
+    }
+
+    @Transactional
+    @Override
+    public TicketWithdrawDtoResponse withdraw(Long walletId, WithdrawDto withdraw) throws WalletException{
+        Wallet wallet = findById(walletId);
+
+        validateFound(wallet, withdraw);
+        substractFounds(wallet, withdraw);
+        //Envia fondos a al cvu proporcionado...
+        
+        //crea el ticket con la informacion
+        return new TicketWithdrawDtoResponse(
+            withdraw.type(),
+            withdraw.amount(),
+            withdraw.cuil(),
+            LocalDateTime.now(),
+            withdraw.keyTransfer()
+        );
+    }
+    private void substractFounds(Wallet wallet, WithdrawDto withdraw) {
+        if(withdraw.type().equals(MoneyType.USD))
+            wallet.setGlobalMoney(wallet.getGlobalMoney()-withdraw.amount());
+        if(withdraw.type().equals(MoneyType.ARS))
+            wallet.setLocalMoney(wallet.getLocalMoney()-withdraw.amount());
+    }
+
+    private boolean validateFound(Wallet wallet, WithdrawDto withdraw) throws WalletException {
+        if(withdraw.amount() <=0 )
+            throw new WalletException("Monto de retiro invalido");
+        if(withdraw.type().equals(MoneyType.USD))
+            return withdraw.amount() <= wallet.getGlobalMoney();
+        if(withdraw.type().equals(MoneyType.ARS))
+            return withdraw.amount() <= wallet.getLocalMoney();
+        
+        throw new WalletException("Fondos insuficientes");
     }
 
     @Override
