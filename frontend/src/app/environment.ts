d322401a72f3cd0a2927b69966e3ba6c38ea7aa6
FrancoNordinelli <imnordi@gmail.com>
2024-04-22 22:51:43 -0300

    prueba integracion railway

--- frontend/src/app/environment.ts
+++ frontend/src/app/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     //apiBaseUrl: 'http://localhost:8080'
     //prueba integracion con deploy back railway
-    apiBaseUrl: 'http://patrickscoins.up.railway.app'
+    apiBaseUrl: 'patrickscoins.up.railway.app'
 }
