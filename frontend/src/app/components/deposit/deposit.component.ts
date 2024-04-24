9cf440831cf3d54cff5d03d20bdbe89173d80bb5
Gastón Cuello <gastonec16@gmail.com>
2024-04-24 07:58:24 -0300

    colocación de html de sweetalert en otro archivo

--- frontend/src/app/components/deposit/deposit.component.ts
+++ frontend/src/app/components/deposit/deposit.component.ts
@@ -6,6 +6,7 @@ import { FooterComponent } from '../footer/footer.component'
 import Swal from 'sweetalert2'
 import { MoneyService } from '../../services/money.service'
 import { FormsModule } from '@angular/forms'
+import { HtmlDeposit } from './html-deposit'
 
 @Component({
     selector: 'app-deposit',
@@ -32,59 +33,7 @@ export class DepositComponent {
         })
         swalWithBootstrapButtons
             .fire({
-                html: `
-      <h2 style="color: var(--yellow);">Ingrese los datos de su tarjeta</h2>
-      <img src="/assets/img/img-creditcard.png"  width="300px" alt=""> <br>
-      <div style="text-align: left; color: #ffffff;">
-      <label>Número de tarjeta:</label>
-      <input id="swal-input1" type="number" 
-      style="background-color: white;
-      color: black;
-      padding: 10px 20px;
-      border-radius: 1rem;
-      font-size: larger;
-      width:-webkit-fill-available;
-      ">
-
-      <div style="display: flex;
-              justify-content: space-between; align-items: center;">
-                <div style="display: flex;
-                flex-direction: column;">
-                  <label>Vence el:</label>
-                  <input id="swal-input2" type="number" 
-                  style="background-color: white;
-                  color: black;
-                  padding: 10px 20px;
-                  border-radius: 1rem;
-                  font-size: larger;
-                  width:70%;">
-                  <small style="color: grey">EJEMPLO: 05-27</small>
-                  </div>
-                  <div style="display: flex;
-                  flex-direction: column; ">
-                  <label>CVV:</label>
-                  <input id="swal-input3" type="number" 
-                  style="background-color: white;
-                  color: black;
-                  padding: 10px 20px;
-                  border-radius: 1rem;
-                  font-size: larger;
-                  width:70%;">
-                  <small style="color: grey">EJEMPLO: 123</small>
-                </div>
-
-              </div>
-      
-      <label>Nombre del titular:</label>
-      <input id="swal-input4" type="text" 
-      style="background-color: white;
-      color: black;
-      padding: 10px 20px;
-      border-radius: 1rem;
-      font-size: larger;
-      width:-webkit-fill-available;">
-     </div>
-    `,
+                html: HtmlDeposit.cardData,
                 preConfirm: () => {
                     var inputValue1 = (<HTMLInputElement>document.getElementById('swal-input1')).value
                     var inputValue2 = (<HTMLInputElement>document.getElementById('swal-input2')).value
@@ -100,85 +49,7 @@ export class DepositComponent {
                     swalWithBootstrapButtons
                         .fire({
                             icon: 'success',
-                            html: `
-          <h2 style="color:white"><span  style="color: var(--yellow);">¡Pago realizado </span>con éxito!</h2>
-          <h3 style="color:white">COMPROBANTE DE DEPÓSITO:</h3>
-          <div style="color:white; text-align:left;">
-          <div style="display: flex;
-          justify-content: space-between;">
-            <div style="display: flex;
-            flex-direction: column;">
-              <label>Monto depositado:</label>
-              <div style="background-color: white;
-          color: black;
-          padding: 10px 20px;
-          border-radius: 1rem;
-          font-size: larger;">
-          <span>500.000</span>
-          </div>
-            </div>
-            <div style="display: flex;
-            flex-direction: column; ">
-              <label>Moneda:</label>
-              <div style="background-color: white;
-          color: black;
-          padding: 10px 20px;
-          border-radius: 1rem;
-          font-size: larger;">
-              <span>ARS</span>
-              </div>
-            </div>
-          </div>
-          <label>Desde:</label>
-          <div style="background-color: white;
-          color: black;
-          padding: 10px 20px;
-          border-radius: 1rem;
-          font-size: larger;">
-          <span id="entidad-value">entidad-value</span>
-          </div>
-          <label>Nombre / Alias:</label>
-          <div style="background-color: white;
-          color: black;
-          padding: 10px 20px;
-          border-radius: 1rem;
-          font-size: larger;">
-          <span id="alias-value">alias-value</span>
-          </div>
-          <div style="display: flex;
-          justify-content: space-between;">
-            
-            <div style="display: flex;
-            flex-direction: column;">
-              <label>Fecha:</label>
-              <div style="background-color: white;
-          color: black;
-          padding: 10px 20px;
-          border-radius: 1rem;
-          font-size: larger;">
-          <span>07 / 07 / 2024</span>
-          </div>
-            </div>
-            
-            <div style="display: flex;
-            flex-direction: column;">
-              <label>Hora:</label>
-              <div style="background-color: white;
-            color: black;
-            padding: 10px 20px;
-            border-radius: 1rem;
-            font-size: larger;">
-              <span>11:11 am</span>
-              </div>
-            </div>
-              </div>
-              </div>
-
-              <h3 style="color:white">¡Gracias por confiar en nuestro servicio! </h3>
-              <h4>
-                  <a  style="color: var(--yellow);">Compartir comprobante. <img src="/assets/img/icon-share.png" alt="" width="24px" height="24px"></a>
-              </h4>
-        `,
+                            html: HtmlDeposit.depositTicket,
                             confirmButtonText: 'Aceptar',
                             background: 'linear-gradient(0deg, rgba(40, 118, 53, 1) 0%, rgba(23, 77, 32, 1) 100%)'
                         })
--- frontend/src/app/components/withdraw/withdraw.component.ts
+++ frontend/src/app/components/withdraw/withdraw.component.ts
@@ -16,6 +16,7 @@ import { FormsModule } from '@angular/forms'
 import { MatInputModule } from '@angular/material/input'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import Swal from 'sweetalert2'
+import { HtmlWithdraw } from './htmlWithdraw'
 
 @Component({
     selector: 'app-withdraw',
@@ -70,29 +71,7 @@ export class WithdrawComponent {
         })
         swalWithBootstrapButtons
             .fire({
-                html: `
-      <h2 style="color: var(--yellow);">Ingrese los datos de retiro</h2>
-      <div style="text-align: left; color: #ffffff;">
-      <label>Ingresa CBU/CVU/Alias:</label>
-      <input id="swal-input1" type="text" 
-      style="background-color: white;
-      color: black;
-      padding: 10px 20px;
-      border-radius: 1rem;
-      font-size: larger;
-      width:-webkit-fill-available;
-      ">
-      
-      <label>Cuil:</label>
-      <input id="swal-input2" type="number" 
-      style="background-color: white;
-      color: black;
-      padding: 10px 20px;
-      border-radius: 1rem;
-      font-size: larger;
-      width:-webkit-fill-available;">
-     </div>
-    `,
+                html: HtmlWithdraw.withdrawForm,
                 preConfirm: () => {
                     var inputValue1 = (<HTMLInputElement>document.getElementById('swal-input1')).value
                     var inputValue2 = (<HTMLInputElement>document.getElementById('swal-input2')).value
@@ -105,38 +84,7 @@ export class WithdrawComponent {
                 if (result.isConfirmed) {
                     swalWithBootstrapButtons
                         .fire({
-                            html: `
-        <h2 style="color: var(--yellow);">Vas a enviar dinero a:</h2>
-        <h3> <span  style="color: var(--yellow);">Nombre Segundo</span> <span style="color: var(--green-3);">Apellido</span></h3>
-        <div style="text-align: left; color: #ffffff;">
-        <label>Alias:</label>
-        <div style="background-color: white;
-        color: black;
-        padding: 10px 20px;
-        border-radius: 1rem;
-        font-size: larger;">
-        <span id="alias-value">alias-value</span>
-        </div>
-      
-        <label>Cuil:</label>
-        <div style="background-color: white;
-        color: black;
-        padding: 10px 20px;
-        border-radius: 1rem;
-        font-size: larger;">
-        <span id="cuil-value">cuil-value</span>
-        </div>
-      
-        <label>Banco entidad:</label>
-        <div style="background-color: white;
-        color: black;
-        padding: 10px 20px;
-        border-radius: 1rem;
-        font-size: larger;">
-        <span id="entidad-value">entidad-value</span>
-        </div>
-        </div>
-        `,
+                            html: HtmlWithdraw.withdrawConfirmation,
                             focusConfirm: false,
                             showCancelButton: true,
                             confirmButtonText: 'Aceptar',
@@ -161,93 +109,7 @@ export class WithdrawComponent {
                                             swalWithBootstrapButtons
                                                 .fire({
                                                     icon: 'success',
-                                                    html: `
-              <h2 style="color:white"><span  style="color: var(--yellow);">¡Retiro realizado </span>con éxito!</h2>
-              <h3 style="color:white">COMPROBANTE DE RETIRO:</h3>
-              <div style="color:white; text-align:left;">
-              <div style="display: flex;
-              justify-content: space-between;">
-                <div style="display: flex;
-                flex-direction: column;">
-                  <label>Monto retirado:</label>
-                  <div style="background-color: white;
-              color: black;
-              padding: 10px 20px;
-              border-radius: 1rem;
-              font-size: larger;">
-              <span>500.000</span>
-              </div>
-                </div>
-                <div style="display: flex;
-                flex-direction: column; ">
-                  <label>Moneda:</label>
-                  <div style="background-color: white;
-              color: black;
-              padding: 10px 20px;
-              border-radius: 1rem;
-              font-size: larger;">
-                  <span>ARS</span>
-                  </div>
-                </div>
-              </div>
-              <label>Desde:</label>
-              <div style="background-color: white;
-              color: black;
-              padding: 10px 20px;
-              border-radius: 1rem;
-              font-size: larger;">
-              <span id="entidad-value">entidad-value</span>
-              </div>
-              <label>Alias:</label>
-              <div style="background-color: white;
-              color: black;
-              padding: 10px 20px;
-              border-radius: 1rem;
-              font-size: larger;">
-              <span id="alias-value">alias-value</span>
-              </div>
-              <label>Cuil:</label>
-              <div style="background-color: white;
-              color: black;
-              padding: 10px 20px;
-              border-radius: 1rem;
-              font-size: larger;">
-              <span id="cuil-value">cuil-value</span>
-              </div>
-              <div style="display: flex;
-              justify-content: space-between;">
-                
-                <div style="display: flex;
-                flex-direction: column;">
-                  <label>Fecha:</label>
-                  <div style="background-color: white;
-              color: black;
-              padding: 10px 20px;
-              border-radius: 1rem;
-              font-size: larger;">
-              <span>07 / 07 / 2024</span>
-              </div>
-                </div>
-                
-                <div style="display: flex;
-                flex-direction: column;">
-                  <label>Hora:</label>
-                  <div style="background-color: white;
-                color: black;
-                padding: 10px 20px;
-                border-radius: 1rem;
-                font-size: larger;">
-                  <span>11:11 am</span>
-                  </div>
-                </div>
-                  </div>
-                  </div>
-
-                  <h3 style="color:white">¡Gracias por confiar en nuestro servicio! </h3>
-                  <h4>
-                      <a  style="color: var(--yellow);">Compartir comprobante. <img src="/assets/img/icon-share.png" alt="" width="24px" height="24px"></a>
-                  </h4>
-            `,
+                                                    html: HtmlWithdraw.withdrawTicket,
                                                     confirmButtonText: 'Aceptar',
                                                     background:
                                                         'linear-gradient(0deg, rgba(40, 118, 53, 1) 0%, rgba(23, 77, 32, 1) 100%)'
